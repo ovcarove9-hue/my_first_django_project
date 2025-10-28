ffc0b6941fb6df03fe3549f8bac2264e4fd7ef6b
ovcarove9-hue <ovcarove9@gmail.com>
2025-10-28 21:58:08 +0300

    wb

--- app/models.py
+++ app/models.py
@@ -25,7 +25,7 @@ class Category(models.Model):
 class Product(models.Model):
     name = models.CharField(verbose_name="Наименование", max_length=255, null=False, blank=False)
     description = models.TextField(verbose_name="Описание", max_length=1000, null=False, blank=False)
-    price = models.DecimalField(verbose_name="Цена", decimal_places=2, max_digits=9, null=False, blank=False)
+    price = models.DecimalField(verbose_name="Цена", decimal_places=2, max_digits=100, null=False, blank=False)
     image_url = models.URLField(verbose_name="Ссылка на изображение", max_length=500, null=False, blank=False)
     category = models.ForeignKey("Category", verbose_name="Категория",null=False, blank=False, on_delete=models.CASCADE)
     brand = models.ForeignKey("Brand", verbose_name="Бренд", null=False, blank=False, on_delete=models.CASCADE)
